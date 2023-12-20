c9b7e058140097386d68efbb3f45d9b05dccde27
AnandLadani20 <anandladani9192@gmail.com>
2023-12-20 11:31:48 +0530

    style in hero page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,32 +4,37 @@ import { Box, Button, Grid, Typography } from "@mui/material";
 import ModalBtn from "../components/common/ModalBtn";
 import Register from "../components/common/Register";
 import SponsorForm from "../components/common/SponsorForm";
+import herobg from '../files/images/council.jpg'
 
 const Home = () => {
   return (
     <>
       <Grid height="100vh" width="100vw" m="0 auto" overflow="auto">
         <Header />
-        <Grid height="calc(100% - 90px)" overflow="auto">
-          <Grid container justifyContent="space-evenly" px="25px" py="25px">
-            <Grid item xs={8} md={4.5}></Grid>
+        <Grid height="calc(100% - 90px)" overflow="auto" sx={{position:"relative"}}>
+          <Box sx={{position:"absolute",zIndex:"-1",height:"100%",width:"100%","::before":{content:"' '",position:"absolute",zIndex:"1",top:0,left:0,width:"100%",height:"100%",backgroundColor:"#1f5cff",opacity:0.7}}}>
+            <img src={herobg} alt="hero-bg" style={{height:"100%",width:"100%",position:"absolute",zIndex:"-2"}}/>
+          </Box>
+          <Grid container justifyContent="space-evenly" height="100%" alignItems="center" px="25px" py="25px">
             <Grid item xs={12} md={4.5}>
-              <Typography variant="h3" fontWeight={600}>
+              <Typography variant="h2" color="#fff" sx={{letterSpacing:"-2px"}} fontWeight={500}>
                 WOMEN DIRECTORS
               </Typography>
-              <Typography variant="h3" fontWeight={600}>
-                CONCLAVE 2022
+              <Typography variant="h2" color="#fff" sx={{letterSpacing:"-2px"}} fontWeight={500}>
+                CONCLAVE 2024
               </Typography>
-              <Typography variant="h3" fontWeight={600}>
-                #WDC2022
+              <Typography variant="h2" color="#fff" sx={{letterSpacing:"-2px"}} fontWeight={500}>
+                #WDC2024
               </Typography>
-              <Typography variant="h5">
+              <Typography variant="h5"  color="#fff">
                 CELEBRATE WOMEN BOARD LEADERS
               </Typography>
               <Button>Book Your Seat</Button>
             </Grid>
+            <Grid item xs={8} md={4.5}></Grid>
           </Grid>
-          <ModalBtn btnName="Book Now">
+        </Grid>
+        <ModalBtn btnName="Book Now">
             <Typography variant="h5">register</Typography>
             <Register />
           </ModalBtn>
@@ -37,7 +42,6 @@ const Home = () => {
             <Typography variant="h5">Please Enter Your Details</Typography>
             <SponsorForm/>
           </ModalBtn>
-        </Grid>
       </Grid>
     </>
   );
